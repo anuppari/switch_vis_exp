6299f7cfda00b348a27d5d47b56d4a29e850fdef
Anup Parikh <anuppari@gmail.com>
2016-02-09 13:21:25 -0500

    Add velocity map and remove extraneous stuff

---
+++ src/turtlebot_control_node.cpp
@@ -0,0 +1,72 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <switch_vis_exp/MapVel.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+class turtlebot_control
+{
+    ros::NodeHandle nh;
+    ros::ServiceClient client;
+    ros::Subscriber poseSub;
+    ros::Publisher velPub;
+    
+    // parameters
+    double kw;
+    
+public:
+    turtlebot_control()
+    {
+        // parameters
+        nh.param<double>("kw",kw,1);
+        
+        // publishers
+        velPub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi",1);
+        
+        // get service handle
+        client = nh.serviceClient<switch_vis_exp::MapVel>("get_velocity");
+        
+        // subscribers
+        poseSub = nh.subscribe("pose",10,&turtlebot_control::poseCB,this);
+    }
+    
+    void poseCB(const geometry_msgs::PoseStampedConstPtr& pose)
+    {
+        switch_vis_exp::MapVel srv;
+        srv.request.pose = pose->pose;
+        if (client.call(srv))
+        {
+            // get velocity
+            Eigen::Vector3d vel;
+            vel << srv.response.twist.linear.x, srv.response.twist.linear.y, srv.response.twist.linear.z;
+            
+            // get orientation of turtlebot
+            Eigen::Quaterniond quat(pose->pose.orientation.x, pose->pose.orientation.y, pose->pose.orientation.z, pose->pose.orientation.w);
+            
+            // rotate velocity into turtlebot body frame
+            Eigen::Vector3d body_vel = quat.inverse()*vel;
+            
+            // non-holonomic controller
+            Eigen::Vector3d xDir(1,0,0);
+            Eigen::Vector3d w = kw*xDir.cross(body_vel); // angular velocity command
+            
+            // publish to turtlebot
+            geometry_msgs::Twist twistMsg;
+            twistMsg.linear.x = body_vel[0];
+            twistMsg.angular.z = w[2];
+            velPub.publish(twistMsg);
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "velocity_map_node");
+    
+    turtlebot_control tc;
+    
+    ros::spin();
+    return 0;
+}
---
+++ src/velocity_map_node.cpp
@@ -0,0 +1,73 @@
+#include <ros/ros.h>
+#include <switch_vis_exp/MapVel.h>
+
+class velocity_map
+{
+    ros::NodeHandle nh;
+    ros::ServiceServer service;
+    
+    // Parameters
+    double a;       // length of track
+    double b;       // width of track
+    double k1;      // overall velocity gain
+    double k2;      // weighting between getting back to track and moving in direction of track
+    int n;          // sharpness and squareness of track
+    
+public:
+    velocity_map()
+    {
+        // Get parameters
+        nh.param<double>("a",a,2);
+        nh.param<double>("b",b,2);
+        nh.param<double>("k1",k1,1);
+        nh.param<double>("k2",k2,0.5);
+        nh.param<int>("n",n,4);
+        
+        // Start service
+        service = nh.advertiseService("get_velocity", &velocity_map::get_velocity,this);
+    }
+
+    bool get_velocity(switch_vis_exp::MapVel::Request &req,switch_vis_exp::MapVel::Response &resp)
+    {
+        // point on map
+        double x = req.pose.position.x;
+        double y = req.pose.position.y;
+        
+        // velocity component toward track
+        double factor = (std::pow(x/a,n) + std::pow(y/b,n) - 1)/(std::pow(x,n) + std::pow(y,n));
+        double up = -factor*x;
+        double vp = -factor*y;
+        
+        // velocity component moving along track
+        double xt = sgn(x)*std::pow((1/(1/std::pow(a,n)+std::pow(y/(x*b),n))),1.0/n); // intersection of track and line from origin to point, i.e. ~closest point on track
+        double yt = (y/x)*xt;
+        double ut = sgn(yt);                        // tangent slope at point
+        double vt = -1*sgn(yt)*std::pow(b/a,n)*std::pow(xt/yt,n-1);
+        double norm = std::pow(std::pow(ut,2)+std::pow(vt,2),0.5);
+        
+        // total velocity
+        double u = k1*(ut/norm + k2*up);
+        double v = k1*(vt/norm + k2*vp);
+        
+        // response
+        resp.twist.linear.x = u;
+        resp.twist.linear.y = v;
+        
+        return 0;
+    }
+
+    int sgn(double val)
+    {
+        return ((val > 0) - (val < 0));
+    }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "velocity_map_node");
+    
+    velocity_map vm;
+    
+    ros::spin();
+    return 0;
+}
