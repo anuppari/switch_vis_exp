da13f9e38bd82ffcdd8fd55ec56f909ac2dd55ca
Anup Parikh <anuppari@gmail.com>
2016-02-11 17:47:51 -0800

    Add track center parameter to velocity map

--- src/velocity_map_node.cpp
+++ src/velocity_map_node.cpp
@@ -9,6 +9,8 @@ class velocity_map
     // Parameters
     double a;       // length of track
     double b;       // width of track
+    double x0;      // track center
+    double y0;      // track center
     double k1;      // overall velocity gain
     double k2;      // weighting between getting back to track and moving in direction of track
     int n;          // sharpness and squareness of track
@@ -17,11 +19,14 @@ public:
     velocity_map()
     {
         // Get parameters
-        nh.param<double>("a",a,1);
-        nh.param<double>("b",b,1);
-        nh.param<double>("k1",k1,0.5);
-        nh.param<double>("k2",k2,0.5);
-        nh.param<int>("n",n,4);
+        ros::NodeHandle nhp("~");
+        nhp.param<double>("a",a,2);
+        nhp.param<double>("b",b,2);
+        nhp.param<double>("x0",x0,0);
+        nhp.param<double>("y0",y0,0);
+        nhp.param<double>("k1",k1,0.5);
+        nhp.param<double>("k2",k2,0.1);
+        nhp.param<int>("n",n,4);
         
         // Start service
         service = nh.advertiseService("get_velocity", &velocity_map::get_velocity,this);
@@ -34,15 +39,16 @@ public:
         double y = req.pose.position.y;
         
         // velocity component toward track
-        double factor = (std::pow(x/a,n) + std::pow(y/b,n) - 1)/(std::pow(x,n) + std::pow(y,n));
-        double up = -factor*x;
-        double vp = -factor*y;
+        double factor = (std::pow((x-x0)/a,n) + std::pow((y-y0)/b,n) - 1)/(std::pow((x-x0),n) + std::pow((y-y0),n));
+        double up = -factor*(x-x0);
+        double vp = -factor*(y-y0);
         
         // velocity component moving along track
-        double xt = sgn(x)*std::pow((1/(1/std::pow(a,n)+std::pow(y/(x*b),n))),1.0/n); // intersection of track and line from origin to point, i.e. ~closest point on track
-        double yt = (y/x)*xt;
-        double ut = sgn(yt);                        // tangent slope at point
-        double vt = -1*sgn(yt)*std::pow(b/a,n)*std::pow(xt/yt,n-1);
+        double m = (y-y0)/(x-x0);
+        double xt = sgn(x-x0)*std::pow((1/(1/std::pow(a,n)+std::pow(m/b,n))),1.0/n) + x0; // intersection of track and line from origin to point, i.e. ~closest point on track
+        double yt = m*(xt-x0) + y0;
+        double ut = sgn(yt-y0);                        // tangent slope at point
+        double vt = -1*sgn(yt-y0)*std::pow(b/a,n)*std::pow(1/m,n-1);
         double norm = std::pow(std::pow(ut,2)+std::pow(vt,2),0.5);
         
         // total velocity
